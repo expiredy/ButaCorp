23d9ccde21b01d5f6a40609f4d1fcfb85705224c
expiredy101 <yartaoley2005@mail.ru>
2021-10-23 17:36:00 +0300

    Resotring lost changes and adding contacts component

--- dord-site/src/components/InformationSections/AboutUsSection.js
+++ dord-site/src/components/InformationSections/AboutUsSection.js
@@ -2,15 +2,16 @@ import "./InformationSection.css";
 import ConesGroup from "./../../content/images/text_view/ConesGroup.png";
 
 function InformationSection(){
-    return(<div id="about" class="container">
+    return(
+    <div id="about" class="container">
     <div class="container__half">
         <img src={ConesGroup} class="image-container" alt="ConesGroup"></img>
     </div>
     <div class="container__half">
         <h1 class="huge-title-h1">Про нас</h1>
-        <h3 class="text-session">Наш проект стартовал в <span>2019 году</span>,
+        <p class="text-session">Наш проект стартовал в <span>2019 году</span>,
                                 когда мы пробовали свои силы в реализации проекта по биотопливу,
-                                но, углубившись в эту тему, решили сосредоточить свои силы на сырьевом бутаноле из возобновляемого источника.</h3>
+                                но, углубившись в эту тему, решили сосредоточить свои силы на сырьевом бутаноле из возобновляемого источника.</p>
     </div>
     </div>)
 }
---
+++ dord-site/src/components/InformationSections/ContactsSection.js
@@ -0,0 +1,43 @@
+import "./InformationSection.css";
+
+import mail_logo from "./../../content/images/contacts/mail_logo.png";
+import vk_logo from "./../../content/images/contacts/vk_logo.png";
+import telegram_logo from "./../../content/images/contacts/telegram_logo.png";
+import phone_number_logo from "./../../content/images/contacts/phone_number_logo.png";
+
+import RoadToNowhere from "./../../content/images/text_view/RoadToNowhere.png";
+
+function ContactsSection(){
+    return(
+        <div id="contacts" className="container">
+        <div class="container__half">
+            <h1 class="huge-title-h1">Контакты</h1>
+            <div className="contact-container">
+                <img src={mail_logo} alt="mail"></img>
+                <div className="contact-element">
+                    <a href="mailto:butacorpinf@gmail.com">butacorpinf@gmail.com</a></div>
+            </div>
+            <div className="contact-container">
+                <img src={vk_logo} alt="vk"></img>
+                <div className="contact-element">
+                    <a href="https://vk.com/vladorob">Владислав Рогозин</a></div>
+            </div>
+            <div className="contact-container">
+                <img src={telegram_logo} alt="telegram"></img>
+                <div className="contact-element">
+                    <a href="https://t.me/Vladorob">Владислав Рогозин</a></div>
+            </div>
+            <div className="contact-container">
+                <img src={phone_number_logo} alt="number"></img>
+                <div className="contact-element">
+                    <a href="tel:+7(980)248-57-25">+7(980)248-57-25</a></div>
+            </div>
+        </div>
+        <div class="container__half">
+            <img src={RoadToNowhere} alt="Road"></img>
+        </div>
+    </div>
+    )
+}
+
+export default ContactsSection;
