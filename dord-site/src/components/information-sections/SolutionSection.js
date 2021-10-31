d001139269fb72b07601dbe0f1ae7565390b671e
expiredy101 <yartaoley2005@mail.ru>
2021-11-01 00:14:02 +0300

    starting adding steps of realization section

---
+++ dord-site/src/components/information-sections/SolutionSection.js
@@ -0,0 +1,24 @@
+import "./InformationSection.css"
+
+function StepsElementsGenerator() {
+    const stepsData = {
+
+    }
+    const STEPS_TOTAL_COUNT = 4;
+    for (var stepIndex = 0; stepIndex < STEPS_TOTAL_COUNT; step++){
+        
+    }
+}
+
+function SolutionSection(){
+
+    stepsContainer = StepsElementsGenerator();
+
+    return(
+        <div id="solution">
+            
+        </div>
+    )
+}
+
+export default SolutionSection;
